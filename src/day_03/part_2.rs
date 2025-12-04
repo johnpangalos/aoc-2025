b6a2f71f5f11e55f4c875e66f42b323600f35e45
John Pangalos <john@pangalos.dev>
2025-12-04 20:28:55 +0100

    more day 3 wip

--- src/day_03/part_2.rs
+++ src/day_03/part_2.rs
@@ -2,7 +2,7 @@
 use std::fs;
 
 fn main() {
-    let filename = "./src/day_03/example.txt";
+    let filename = "./src/day_03/input.txt";
     let file = fs::read_to_string(filename);
     let contents = match file {
         Ok(contents) => contents,
@@ -10,73 +10,79 @@ fn main() {
     };
 
     let content_str = contents.as_str().trim();
-    let batteries = content_str.split("\n");
-    // let mut sum = 0;
-    for battery in batteries {
-        println!("{}", battery);
-        let arr_str = battery.split("").filter(|x| !x.is_empty());
-        let arr = arr_str
-            .map(|v| match v.parse::<isize>() {
-                Ok(val) => val,
-                Err(_) => panic!("There is something wrong with v: {}", v),
-            })
-            .collect::<Vec<isize>>();
+    let mut batteries = content_str.split("\n");
+    let mut sum = 0;
+    let battery = batteries.next().unwrap();
+    // for battery in batteries {
+    let arr_str = battery.split("").filter(|x| !x.is_empty());
+    let arr = arr_str
+        .map(|v| match v.parse::<isize>() {
+            Ok(val) => val,
+            Err(_) => panic!("There is something wrong with v: {}", v),
+        })
+        .collect::<Vec<isize>>();
 
-        let mut num: Vec<isize> = vec![];
-        let mut idx = 0;
-        for val in &arr {
-            if num.len() == 0 {
-                num.push(*val);
-                continue;
-            }
-            let t = num.iter().map(|x| x.to_string()).collect::<Vec<String>>();
-            println!("{}", t.join(""));
-            println!();
+    let mut num: Vec<isize> = vec![];
 
-            // let mut temp = true;
-            // let mut buf: Vec<isize> = vec![];
-            let mut k = 0;
-            while k < arr.len() - 1 {
-                println!("k: {}", k);
-                k = k + 1;
-                let tmp = num.iter().map(|x| x.to_string()).collect::<Vec<String>>();
-                println!("curr: {}", tmp.join(""));
-                let temp2 = match num.pop() {
-                    Some(v) => v,
-                    None => panic!("ahhhhh, I'm panicing"),
-                };
-                if temp2 >= *val && num.len() == 11 {
-                    num.push(temp2);
-                    break;
-                }
+    let mut budget = 0;
+    let max_budget = arr.len() - 12;
+    println!("max budget: {}", max_budget);
 
-                if temp2 >= *val {
-                    num.push(temp2);
-                    num.push(*val);
-                    break;
-                }
-                println!("{}, {}, {}", num.len(), arr.len(), k);
-                if num.len() == 0 || 11 - num.len() <= arr.len() - k {
-                    num.push(*val);
-                    break;
-                }
-                // }
-                // if temp2 >= *val {
-                //     num.push(temp2);
-                //     num.push(*val);
-                //     // temp = false;
-                // } else {
-                //     buf.push(temp2);
-                // }
-            }
-            // if num.len() < 12 {
-            //     num.push(*val);
-            // }
-            idx = idx + 1;
+    // 3133322312313332336153233333232281412234221222433272332313372222212233114622232233232321251122522243
+    for val in &arr {
+        if num.len() == 0 {
+            num.push(*val);
+            continue;
         }
-        let out = num.iter().map(|x| x.to_string()).collect::<Vec<String>>();
-        println!("out: {}", out.join(""));
-        println!("---------------------");
+
+        loop {
+            if budget >= max_budget {
+                num.push(*val);
+                println!("path: 1");
+                break;
+            }
+            let prev = match num.pop() {
+                Some(v) => v,
+                None => panic!("ahhhhh, I'm panicing"),
+            };
+            println!("prev: {}, val: {}, len: {}", prev, *val, num.len());
+            if prev >= *val && num.len() == 11 {
+                num.push(prev);
+                println!("path: 2");
+                break;
+            }
+
+            if prev >= *val {
+                num.push(prev);
+                num.push(*val);
+                println!("path: 3");
+                break;
+            }
+            if num.len() == 0 {
+                println!("path: 4");
+                num.push(*val);
+                budget = budget + 1;
+                break;
+            }
+            budget = budget + 1;
+        }
+        let out = num
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join("");
+        println!();
+        println!("val: {}", *val);
+        println!("out: {}", out);
     }
-    // println!("{}", sum)
+    println!();
+    let out = num
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join("");
+    println!("{}", out);
+    sum = sum + out.parse::<usize>().unwrap();
+    // }
+    println!("{}", sum)
 }
