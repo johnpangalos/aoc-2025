17bc8c7f51a9f5d31cb5ed8d834a17e1ef551a02
John Pangalos <john@pangalos.dev>
2025-12-06 12:57:03 +0100

    day 05

--- src/day_05/part_2.rs
+++ src/day_05/part_2.rs
@@ -24,25 +24,71 @@ fn main() {
         };
         (start, end)
     });
+
     let mut sum = 0;
-    let mut prev: Vec<(isize, isize)> = vec![];
+    let mut previous: Vec<(isize, isize)> = vec![];
     for rule in rules.clone() {
-        let mut min = rule.0;
-        let mut max = rule.1;
-        for p in prev.clone() {
-            if min >= p.0 && min <= p.1 {
-                min = p.1 + 1;
-            }
-            if max >= p.0 && max <= p.1 {
-                max = p.0 - 1;
+        // let mut min = rule.0;
+        // let mut max = rule.1;
+        let mut ranges: Vec<(isize, isize)> = vec![rule];
+        for prev in previous.clone() {
+            for new in ranges.clone() {
+                // case 1: miss -> do nothing
+                if new.1 < prev.0 || new.0 > prev.1 {
+                    continue;
+                }
+                // case 2: prev encompasses new -> remove new from ranges
+                if new.1 <= prev.1 && new.0 >= prev.0 {
+                    let to_remove_idx = ranges.iter().position(|x| *x == new).unwrap();
+                    ranges.remove(to_remove_idx);
+                    continue;
+                }
+
+                // case 3: prev overlaps lower new -> combine and expand lower range
+                if prev.1 >= new.0 && prev.0 <= new.0 && prev.1 < new.1 {
+                    let prev_remove_idx = match previous.iter().position(|x| *x == prev) {
+                        Some(v) => v,
+                        None => panic!("({} {}) not in ranges", prev.0, prev.1),
+                    };
+                    let new_remove_idx = match ranges.iter().position(|x| *x == new) {
+                        Some(v) => v,
+                        None => panic!("({} {}) not in ranges", new.0, new.1),
+                    };
+                    previous.remove(prev_remove_idx);
+                    ranges.remove(new_remove_idx);
+                    ranges.push((prev.0, new.1));
+                    continue;
+                }
+
+                // case 4: prev overlaps upper new -> combine and expand uppper range
+                if prev.1 >= new.1 && prev.0 <= new.1 && prev.0 >= new.0 {
+                    let prev_remove_idx = match previous.iter().position(|x| *x == prev) {
+                        Some(v) => v,
+                        None => panic!("({} {}) not in ranges", prev.0, prev.1),
+                    };
+                    let new_remove_idx = match ranges.iter().position(|x| *x == new) {
+                        Some(v) => v,
+                        None => panic!("({} {}) not in ranges", new.0, new.1),
+                    };
+                    previous.remove(prev_remove_idx);
+                    ranges.remove(new_remove_idx);
+                    ranges.push((new.0, prev.1));
+                    continue;
+                }
+
+                // case 5: new encompasses prev -> remove prev
+                if new.1 >= prev.1 && new.0 <= prev.0 {
+                    let prev_remove_idx = match previous.iter().position(|x| *x == prev) {
+                        Some(v) => v,
+                        None => panic!("({} {}) not in ranges", prev.0, prev.1),
+                    };
+                    previous.remove(prev_remove_idx);
+                    continue;
+                }
             }
         }
-        println!("{} {} {}", min, max, max - min + 1);
-        if (max - min) < 0 {
-            continue;
-        }
-        sum = sum + max - min + 1;
-        prev.push((min, max));
+        ranges.iter().for_each(|r| previous.push(*r));
     }
+    previous.iter().for_each(|p| sum = sum + p.1 - p.0 + 1);
     println!("{}", sum);
 }
